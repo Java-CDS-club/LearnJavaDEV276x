f8a09a00b86486377b31dcca75a44734be80efdd
Michael02lin <michael02lin@gmail.com>
2018-11-06 19:44:25 +0800

    edits1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorld {
         System.out.println("This escape sequence \\ turns into a single slash");
         System.out.println("This escape sequence \"creates quotes\"");
         System.out.println("This escape sequence \tmakes a tab indent");
+        System.out.println("tesawasdfadadt");
     }
     public static void printControlFlow(){
         System.out.println("Calling on a method.");
