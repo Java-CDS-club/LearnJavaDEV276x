4cbe5d7bf577d434963f94c304b2a9362c2336b4
Michael02lin <michael02lin@gmail.com>
2018-11-07 14:22:52 +0800

    7 November 2018 2:22:51PM

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,27 +8,28 @@ public class HelloWorld {
 
     //main method, this is the method that the computer executes in the console
     public static void main(String[] args){
-        printEscapeSequences();
-        printControlFlow();
-        showJavaDataTypes();
+        usingEscapeSequences();
+        usingControlFlow();
+        usingDataTypes();
         usingVariables();
-        usingMathOperations();
+        usingMathematicalOperations();
+        usingStringConcatenations();
         System.out.println(12 / 7 * 4.4 * 2 / 4);
     }
 
     //MODULE 1 | BASIC JAVA COMMANDS
-    public static void printEscapeSequences(){
+    public static void usingEscapeSequences(){
         System.out.println("This escape sequence \nmakes a new line");
         System.out.println("This escape sequence \\ turns into a single slash");
         System.out.println("This escape sequence \"creates quotes\"");
         System.out.println("This escape sequence \tmakes a tab indent");
     }
-    public static void printControlFlow() {
+    public static void usingControlFlow() {
         System.out.println("Calling on a method.");
     }
 
     //MODULE 1 | DATA TYPES AND VARIABLES
-    public static void showJavaDataTypes(){
+    public static void usingDataTypes(){
         System.out.println("The Java data types are \nint(integers):213,3,-51,etc. \ndouble(real numbers):23.1,0.5,-1.4, etc.");
         System.out.println("char(one character):'A', '1', 'z', '%' \nboolean(true or false):true, false");
     }
@@ -37,7 +38,7 @@ public class HelloWorld {
         int Computers = 2;
         System.out.println("I have " + Computers + " computers");
     }
-    public static void usingMathOperations(){
+    public static void usingMathematicalOperations(){
         //PEMDAS applies
         //treat modulus % as multiplication and division
         //<!>if int/int=double value, take the number before decimal without rounding, e.g. 15/4=3.75 =3, not 4
@@ -53,6 +54,23 @@ public class HelloWorld {
         y *= 10; //y = y*10(using += -= /= %= works too), "10" can be an "integer" e.g. y+=x is y=y+x
         System.out.println("x add one = " + x);
         System.out.println("y multiplied by 10 = " + y);
+    }
+    public static void usingStringConcatenations(){
+        String var1 = "Michael";
+        String var2 = "Lin";
+        System.out.println(var1 + var2); //results in MichaelLin
+        System.out.println(var1 + " " + var2); //results in Michael LIn
+        //any other data type with String results in String
+        String num1 = "1";
+        System.out.println(1 + 1); //results in 2
+        System.out.println(num1 + 1); //results in "11"
+        String var3 = "is";
+        System.out.println(var3 + " " + true); //results in "is true"
+        //PEMDAS applies
+        System.out.println(1 + 2 + "3" + 4 + 5); //results in "3345"
+        System.out.println(1 * 2 + "3" + 4 * 5); //results in "2320"
+
+
     }
 
 
